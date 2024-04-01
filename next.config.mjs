6543468180a7bf658dc59bc605c4c40191b511d4
Shariar2006 <shariarnafis86@gmail.com>
2024-04-01 16:56:24 +0600

    create sport component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["i.ibb.co", "assets.stickpng.com"],       
+    },
+};
 
 export default nextConfig;
