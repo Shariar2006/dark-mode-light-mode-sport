64f8bd24a49538c346ea2243d64c655bef91bf7d
Shariar2006 <shariarnafis86@gmail.com>
2024-04-02 19:11:50 +0600

    add dark mone and light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Sports from "./Sports";
 import CollectionSpotlight from "./CollectionSpotlight";
+import ThemeSwitch from "./ThemeSwitch";
 
 export default function Home() {
   return (
     <main className="m-20">
+      <div className="flex justify-between items-center">
       <header className="text-2xl font-bold underline">Sports</header>
+
+<ThemeSwitch/>
+      </div>
 
       <Sports/>
       <CollectionSpotlight/>
