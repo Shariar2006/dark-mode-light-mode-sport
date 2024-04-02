b2b28ac1e603c2c011626f61c31cbdb47fa3e8f7
Shariar2006 <shariarnafis86@gmail.com>
2024-04-02 20:02:40 +0600

    project finish

--- src/app/CollectionSpotlight.tsx
+++ src/app/CollectionSpotlight.tsx
@@ -8,51 +8,51 @@ const CollectionSpotlight = () => {
             <p className="text-center mt-5">Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable <br /> experience. Grab yours today!</p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 max-w-[900px] mx-auto">
-                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl">
+                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl dark:shadow-xl dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/f27Cn4B/Ticket-Mockup-3-1.png' alt="image" width={246.88} height={100}></Image>
                     <div className="border-dashed border mb-3 w-[190px] mx-auto">
                     </div>
                     <div className="relative">
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-r-full -mt-8 -left-3"></div>
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-l-full -mt-8 -right-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-r-full -mt-6 -left-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-l-full -mt-6 -right-3"></div>
                     </div>
                     <div className="text-center">
                         <h1 className="text-lg font-bold">Las Vegas Aviators</h1>
                         <h1 className="text-sm mt-3">Oct 15 | Sun | 4:30 PM</h1>
-                        <p className="text-sm text-gray-500 my-3">Las Vegas Ballpark, Las Vegas, Nevada</p>
-                        <button className="text-white bg-black py-1 text-sm px-10">Take Flight Collection</button>
+                        <p className="text-sm text-gray-500 dark:text-gray-300 my-3">Las Vegas Ballpark, Las Vegas, Nevada</p>
+                        <button className="text-white bg-black dark:bg-[#191919] py-1 text-sm px-10">Take Flight Collection</button>
 
                     </div>
                 </div>
-                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl">
+                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl dark:shadow-xl dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/6wm4L5Y/unnamed-1.png' alt="image" width={246.88} height={100}></Image>
                     <div className="border-dashed border mb-3 w-[190px] mx-auto">
                     </div>
                     <div className="relative">
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-r-full -mt-8 -left-3"></div>
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-l-full -mt-8 -right-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-r-full -mt-6 -left-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-l-full -mt-6 -right-3"></div>
                     </div>
                     <div className="text-center">
                         <h1 className="text-lg font-bold">Sacramento River Cats</h1>
                         <h1 className="text-sm mt-3">Oct 15 | Sun | 4:30 PM</h1>
-                        <p className="text-sm text-gray-500 my-3">Sutter Health Park, Sacramento, California</p>
-                        <button className="text-white bg-black py-1 text-sm px-10">Orange Collection</button>
+                        <p className="text-sm text-gray-500 dark:text-gray-300 my-3">Sutter Health Park, Sacramento, California</p>
+                        <button className="text-white bg-black dark:bg-[#191919] py-1 text-sm px-10">Orange Collection</button>
 
                     </div>
                 </div>
-                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl">
+                <div className="p-3 h-[571px] w-[258.88px] shadow-2xl dark:shadow-xl dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/f27Cn4B/Ticket-Mockup-3-1.png' alt="image" width={246.88} height={100}></Image>
                     <div className="border-dashed border mb-3 w-[190px] mx-auto">
                     </div>
                     <div className="relative">
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-r-full -mt-8 -left-3"></div>
-                        <div className="dividerCircle absolute w-5 h-10 bg-gray-200 rounded-l-full -mt-8 -right-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-r-full -mt-6 -left-3"></div>
+                        <div className="dividerCircle darkDividerCircle absolute w-3 h-6 bg-gray-200 rounded-l-full -mt-6 -right-3"></div>
                     </div>
                     <div className="text-center">
                         <h1 className="text-lg font-bold">Las Vegas Aviators</h1>
                         <h1 className="text-sm mt-3">Oct 15 | Sun | 4:30 PM</h1>
-                        <p className="text-sm text-gray-500 my-3">Las Vegas Ballpark, Las Vegas, Nevada</p>
-                        <button className="text-white bg-black py-1 text-sm px-10">Take Flight Collection</button>
+                        <p className="text-sm text-gray-500 dark:text-gray-300 my-3">Las Vegas Ballpark, Las Vegas, Nevada</p>
+                        <button className="text-white bg-black dark:bg-[#191919] py-1 text-sm px-10">Take Flight Collection</button>
 
                     </div>
                 </div>
--- src/app/Sports.tsx
+++ src/app/Sports.tsx
@@ -5,63 +5,63 @@ const Sports = () => {
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 mt-5">
-                <div className="p-3 h-[552px] w-[258.88px] shadow-xl dark:shadow-gray-800 dark:bg-gray-200">
+                <div className="p-3 h-[552px] w-[258.88px] shadow-xl  dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/59D3vxZ/277307281-10158751675643224-148609460173032344-n-1.png' alt="image" width={246.88} height={100}></Image>
                     <h1 className="mt-3 font-medium">Sacramento River Cats</h1>
-                    <div className="dark:bg-gray-700 bg-gray-100 py-3 mt-3">
+                    <div className="dark:bg-[#191919] bg-gray-100 py-3 mt-3">
                         <div className="flex justify-around dark:text-white items-center text-xs text-gray-400">
                             <h3>total events</h3>
                             <h3>sport</h3>
                         </div>
-                        <div className="flex justify-around items-center dark:text-gray-500 text-base font-semibold">
+                        <div className="flex justify-around items-center dark:text-gray-00 text-base font-semibold">
                             <h2>48 Events</h2>
                             <h2>Baseball</h2>
                         </div>
                     </div>
                 </div>
-                <div className="p-3 h-[552px] w-[258.88px] shadow-xl dark:shadow-gray-800 dark:bg-gray-200">
+                <div className="p-3 h-[552px] w-[258.88px] shadow-xl  dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/KyX3PXC/370265821-812394460893172-8391088016650457893-n-1.png' alt="image" width={246.88} height={100}></Image>
                     <h1 className="mt-3 font-medium">Las Vegas Aviators</h1>
-                    <div className="dark:bg-gray-700 bg-gray-100 py-3 mt-3">
+                    <div className="dark:bg-[#191919] bg-gray-100 py-3 mt-3">
                         <div className="flex justify-around dark:text-white items-center text-xs text-gray-400">
                             <h3>total events</h3>
                             <h3>sport</h3>
                         </div>
-                        <div className="flex justify-around items-center dark:text-gray-500 text-base font-semibold">
+                        <div className="flex justify-around items-center dark:text-gray-00 text-base font-semibold">
                             <h2>28 Events</h2>
                             <h2>Baseball</h2>
                         </div>
                     </div>
                 </div>
-                <div className="p-3 h-[552px] w-[258.88px] shadow-xl dark:shadow-gray-800 dark:bg-gray-200">
+                <div className="p-3 h-[552px] w-[258.88px] shadow-xl  dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/RCrtD2Z/395319316-1098948761258832-1320994362450036961-n-1.png' alt="image" width={246.88} height={100}></Image>
                     <h1 className="mt-3 font-medium">New Jersey Devils</h1>
-                    <div className="dark:bg-gray-700 bg-gray-100 py-3 mt-3">
+                    <div className="dark:bg-[#191919] bg-gray-100 py-3 mt-3">
                         <div className="flex justify-around dark:text-white items-center text-xs text-gray-400">
                             <h3>total events</h3>
                             <h3>sport</h3>
                         </div>
-                        <div className="flex justify-around items-center dark:text-gray-500 text-base font-semibold">
+                        <div className="flex justify-around items-center dark:text-gray-00 text-base font-semibold">
                             <h2>15 Events</h2>
                             <h2>ice hockey</h2>
                         </div>
                     </div>
                 </div>
-                <div className="p-3 h-[552px] w-[258.88px] shadow-xl dark:shadow-gray-800 dark:bg-gray-200">
+                <div className="p-3 h-[552px] w-[258.88px] shadow-xl  dark:bg-[#2b2b2b]">
                     <Image src='https://i.ibb.co/KyX3PXC/370265821-812394460893172-8391088016650457893-n-1.png' alt="image" width={246.88} height={100}></Image>
                     <h1 className="mt-3 font-medium">Las Vegas Aviator</h1>
-                    <div className="dark:bg-gray-700 bg-gray-100 py-3 mt-3">
+                    <div className="dark:bg-[#191919] bg-gray-100 py-3 mt-3">
                         <div className="flex justify-around dark:text-white items-center text-xs text-gray-400">
                             <h3>total events</h3>
                             <h3>sport</h3>
                         </div>
-                        <div className="flex justify-around items-center dark:text-gray-500 text-base font-semibold">
+                        <div className="flex justify-around items-center dark:text-gray-00 text-base font-semibold">
                             <h2>48 Events</h2>
                             <h2>Baseball</h2>
                         </div>
                     </div>
                 </div>
-                <div className="relative p-3 h-[552px] w-[258.88px] shadow-xl">
+                <div className="relative p-3 h-[552px] w-[258.88px] shadow-xl dark:bg-[#2b2b2b]">
                     <h3 className='absolute w-14 py-1 bg-black text-white text-center right-3'>Ad</h3>
                     <Image src='https://i.ibb.co/Rc9j3h9/unnamed-1-1.png' alt="image" width={246.88} height={100}></Image>
                     <h1 className="mt-3 text-xl font-bold">Advertisement title</h1>
